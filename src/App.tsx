bb187a58e9a4865ff82ba873f8f070161692207a
Pablo Vicente <pablo.vicente@outlook.com.br>
2023-09-07 12:58:03 -0300

    Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,10 @@
-import { Box } from "@mui/material";
-import { Rank } from "./components/Rank";
-import { SortedCuponsList, ISortedCupon } from "./components/SortedCuponsList";
+import { Dashboard } from "./pages/dashboard";
 
 
 function App() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'green' }}>
-      <SortedCuponsList sotedCupons={sotedCupons} />
-      <Rank sotedCupons={sotedCupons} />
-    </Box>
-
+    <Dashboard />
   );
 }
 
 export default App;
-const sotedCupons: ISortedCupon[] = [
-  {
-    Code: 4000,
-    Name: "507 - HERCILIO SCHATZ"
-  },
-  {
-    Code: 4001,
-    Name: "602 - LOURENO"
-  },
-  {
-    Code: 4001,
-    Name: "507 - HERCILIO SCHATZ"
-  },
-  {
-    Code: 4001,
-    Name: "602 - LOURENO"
-  },
-  {
-    Code: 4002,
-    Name: "604 - HAGATA"
-  },
-  {
-    Code: 4003,
-    Name: "604 - HAGATA"
-  },
-  {
-    Code: 40,
-    Name: "507 - HERCILIO SCHATZ"
-  },
-]
