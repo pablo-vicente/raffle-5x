fcb65f2f54cfcd1a78e0a0dc261846084b938e46
Pablo Vicente <pablo.vicente@outlook.com.br>
2023-09-09 12:22:12 -0300

    Using hook

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,40 +3,26 @@ import { Box, Button, ButtonGroup, Paper } from "@mui/material";
 import { Panel } from "../../components/Panel";
 import { Rank } from "../../components/Rank";
 import { ISortedCupon, SortedCuponsList } from "../../components/SortedCuponsList";
-import { useState } from "react";
-import useRevealCupom from "../../hooks/revealCupom";
+import useRevealCoupon, { RaffleRevealNumbers } from "../../hooks/raffleCoupon";
 
 export function Dashboard() {
-    const [cupom, setCupom] = useState<number>(0);
-    const { randomNumber, setRevealCupom } = useRevealCupom();
-
-    function randomIntFromInterval(min: number, max: number) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-
-    function handleClick() {
-
-        const sortedNumber = randomIntFromInterval(1, 99999)
-        setRevealCupom(sortedNumber);
-        setCupom(sortedNumber);
-    }
+    const { coupon, sorting, start } = useRevealCoupon(RaffleRevealNumbers.LeftToRight, 1, 45000);
 
     const buttons = [
-        <Button key="one" disabled={Number(randomNumber) !== cupom} onClick={handleClick}>Sortear 1</Button>,
-        <Button key="two" disabled={Number(randomNumber) !== cupom}>Sortear Até Acabar</Button>,
+        <Button key="one" disabled={sorting} onClick={() => {
+            start();
+        }}>Sortear 1</Button>,
+        <Button key="two" disabled={sorting}>Sortear Até Acabar</Button>,
     ];
 
-
     return (
-
-
         <Box sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'success.main', gap: '1vh', padding: '1vh' }}>
             <Paper sx={{ width: '100%', height: '100vh', overflowY: 'auto', maxWidth: '30%' }}>
                 <SortedCuponsList sotedCupons={sotedCupons} />
             </Paper>
 
             <Paper elevation={3} sx={{ width: '40%' }}>
-                <Panel cupom={randomNumber} />
+                <Panel coupon={coupon} />
 
                 <Box
                     sx={{
