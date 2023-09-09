1f0a2a76b92ac8bbe2a62517d47f64a8b9e8c49f
Pablo Vicente <pablo.vicente@outlook.com.br>
2023-09-09 12:21:33 -0300

    Panel

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -5,13 +5,13 @@ import {
 } from "@mui/material";
 
 
-export function Panel({ cupom }: { cupom: string }) {
+export function Panel({ coupon }: { coupon: number | string }) {
     return (
 
         <Box sx={{ padding: '2vh' }}>
             <Paper elevation={3} sx={{ height: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Typography variant="h1" sx={{ fontFamily: 'Algerian' }}>
-                    {cupom}
+                    {coupon}
                 </Typography>
             </Paper>
         </Box>
