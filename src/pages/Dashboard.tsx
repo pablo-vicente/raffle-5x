73b5a4e26960e0a3718dc5449eff396d22555d2c
Pablo Vicente <pablo.vicente@outlook.com.br>
2023-12-30 12:34:07 -0300

    Remove

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Paper, Typography } from "@mui/material";
+import { Box, Button, ButtonGroup, Paper } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { RaffledCouponsList } from "../components/RaffledCouponsList";
 import { Rank, RankDisplay } from "../components/Rank";
